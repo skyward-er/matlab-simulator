d9bc871b65be0840788b7fc3b088f5e615caee74
Filippo Inno <filippo.inno@skywarder.eu>
2021-02-22 14:22:27 +0100

    Update manageSignalFrequencies.m

--- ControlSimulator/manageSignalFrequencies.m
+++ ControlSimulator/manageSignalFrequencies.m
@@ -129,7 +129,6 @@ if freq.magnetometerFrequency > freq.controlFrequency
         sensorData.magnetometer.measure = quatrotate(Q, magnFieldInertial);
     else
         sensorData.magnetometer.measure = repmat(magneticFieldApprox(0)', N, 1);
-        a = sensorData.magnetometer.measure
     end
 else
     z = -Y(end, 3);
