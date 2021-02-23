e7182e440ae4e4cbeef6b807e572694361321eac
Alessqndro Del Duca <alessandro.delduca@skywarder.eu>
2021-02-23 18:16:43 +0100

    Changed magnetometer object name

--- ControlSimulator/initSensors.m
+++ ControlSimulator/initSensors.m
@@ -64,15 +64,15 @@ GPS_NEOM9N.transMatrix=diag([1 1 1]); % axis transformation
 
 % initial megnetometer sensor from LSM9DS1
 MAGN_IIS2MDC=Sensor3D(); % magnetic field in mgauss, temp should be in C°-25C°
-MAGN_LSM9DS1.maxMeasurementRange=49152; % in mgauss
-MAGN_LSM9DS1.minMeasurementRange=-49152; % in mgauss
-MAGN_LSM9DS1.resolution=1.5; % in mgauss
-MAGN_LSM9DS1.tempOffset=0; % +-0.3 in mgauss
-MAGN_LSM9DS1.noiseVariance=9; % guess in mgauss
-MAGN_LSM9DS1.offsetX=0; % +-60 in mgauss
-MAGN_LSM9DS1.offsetY=0; % +-60 in mgauss
-MAGN_LSM9DS1.offsetZ=0; % +-60 in mgauss
-MAGN_LSM9DS1.walkDiffusionCoef=1; % guess
-MAGN_LSM9DS1.dt=0.01; % sampling time
-MAGN_LSM9DS1.transMatrix=diag([1 1 1]); % axis transformation
+MAGN_IIS2MDC.maxMeasurementRange=49152; % in mgauss
+MAGN_IIS2MDC.minMeasurementRange=-49152; % in mgauss
+MAGN_IIS2MDC.resolution=1.5; % in mgauss
+MAGN_IIS2MDC.tempOffset=0; % +-0.3 in mgauss
+MAGN_IIS2MDC.noiseVariance=9; % guess in mgauss
+MAGN_IIS2MDC.offsetX=0; % +-60 in mgauss
+MAGN_IIS2MDC.offsetY=0; % +-60 in mgauss
+MAGN_IIS2MDC.offsetZ=0; % +-60 in mgauss
+MAGN_IIS2MDC.walkDiffusionCoef=1; % guess
+MAGN_IIS2MDC.dt=0.01; % sampling time
+MAGN_IIS2MDC.transMatrix=diag([1 1 1]); % axis transformation
 
