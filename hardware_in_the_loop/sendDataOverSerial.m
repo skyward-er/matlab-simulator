f4c0bee64d0929ed2c37f7b5e3c6e52d38a2eae4
Alberto Nidasio <alberto.nidasio@skywarder.eu>
2022-09-05 16:34:01 +0200

    Updated plots script

--- hardware_in_the_loop/sendDataOverSerial.m
+++ hardware_in_the_loop/sendDataOverSerial.m
@@ -1,6 +1,6 @@
 %{
 
-SEND DATA OVER SERIAL - use the serial to send flight phases flags and 
+SEND DATA OVER SERIAL - use the serial to send flight phases flags and
                         sensors and velocities data to the microcontroller
 
 Author: Emilio Corigliano
@@ -11,35 +11,32 @@ Release date: 17/08/2022
 %}
 
 function [] = sendDataOverSerial(data, flags)
+    dataToBeSent.accelerometer = data.accelerometer.measures;
+    dataToBeSent.gyro = data.gyro.measures;
+    dataToBeSent.magnetometer = data.magnetometer.measures;
 
-dataToBeSent.accelerometer = data.accelerometer.measures;
-dataToBeSent.gyro = data.gyro.measures;
-dataToBeSent.magnetometer = data.magnetometer.measures;
+    dataToBeSent.gps.positionMeasures = [data.gps.latitude, data.gps.longitude, data.gps.positionMeasures(3)];
+    dataToBeSent.gps.velocityMeasures = data.gps.velocityMeasures;
+    dataToBeSent.gps.fix = data.gps.fix;
+    dataToBeSent.gps.nsat = data.gps.nsat;
 
-dataToBeSent.gps.positionMeasures = data.gps.positionMeasures;
-dataToBeSent.gps.velocityMeasures = data.gps.velocityMeasures;
-dataToBeSent.gps.fix = data.gps.fix;
-dataToBeSent.gps.nsat = data.gps.nsat;
+    dataToBeSent.barometer = data.barometer.measures;
+    dataToBeSent.pitot = data.pitot.measures;
 
-dataToBeSent.barometer = data.barometer.measures;
-dataToBeSent.pitot = data.pitot.measures;
+    dataToBeSent.temperature = data.barometer.temperature(1);
 
-dataToBeSent.temperature = data.barometer.temperature(1);
+    dataToBeSent.kalman.z = data.kalman.z;
+    dataToBeSent.kalman.vz = data.kalman.vz;
+    dataToBeSent.kalman.vMod = data.kalman.vMod;
 
-dataToBeSent.kalman.z = data.kalman.z;
-dataToBeSent.kalman.vz = data.kalman.vz;
-dataToBeSent.kalman.vMod = data.kalman.vMod;
+    dataToBeSent.flags.flagFligth = cast(flags(1), "double");
+    dataToBeSent.flags.flagAscent = cast(flags(2), "double");
+    dataToBeSent.flags.flagBurning = cast(flags(3), "double");
+    dataToBeSent.flags.flagAeroBrakes = cast(flags(4), "double");
+    dataToBeSent.flags.flagPara1 = cast(flags(5), "double");
+    dataToBeSent.flags.flagPara2 = cast(flags(6), "double");
 
+    arrayToBeSent = structToSingles(dataToBeSent);
 
-dataToBeSent.flags.flagFligth = cast(flags(1), "double");
-dataToBeSent.flags.flagAscent = cast(flags(2), "double");
-dataToBeSent.flags.flagBurning = cast(flags(3), "double");
-dataToBeSent.flags.flagAeroBrakes = cast(flags(4), "double");
-dataToBeSent.flags.flagPara1 = cast(flags(5), "double");
-dataToBeSent.flags.flagPara2 = cast(flags(6), "double");
-
-arrayToBeSent = structToSingles(dataToBeSent);
-
-serialbridge("Write", arrayToBeSent);
-
+    serialbridge("Write", arrayToBeSent);
 end
--- simulator/plots.m
+++ simulator/plots.m
@@ -1,65 +1,67 @@
 %% Control variable: servo angle
-figures.servo_angle = figure('Name','Servo angle after burning phase');
+figures.servo_angle = figure('Name', 'Servo angle after burning phase');
 plot(c.Tf_tot, c.ap_tot);
 grid on;
 xlabel('Time [s]');
 ylabel('\alpha [rad]');
 title('Servo angle');
-exportgraphics(figures.servo_angle,'report_images\servo_angle.pdf','ContentType','vector')
+% exportgraphics(figures.servo_angle,'report_images\servo_angle.pdf','ContentType','vector')
 
 %% Control variable: servo angle
-ap_tot_rescale = rescale(c.ap_tot,"InputMin",0,"InputMax",settings.servo.maxAngle);
-figures.servo_control_action = figure('Name','Servo angle after burning phase');
-plot(c.Tf_tot, ap_tot_rescale);
+% ap_tot_rescale = rescale(c.ap_tot, "InputMin", 0, "InputMax", settings.servo.maxAngle);
+figures.servo_control_action = figure('Name', 'Servo angle after burning phase');
+plot(c.ap_ref_tot);
 grid on;
 xlabel('Time [s]');
-ylabel('extension [%]');
+ylabel('Extension [%]');
 title('Servo control action');
-exportgraphics(figures.servo_control_action,'report_images\control_action.pdf','ContentType','vector')
+% exportgraphics(figures.servo_control_action,'report_images\control_action.pdf','ContentType','vector')
+
 %% Airbrake surface
-figures.arb_exposed_surface = figure('Name','Airbrake exposed surface');
-c.dS = settings.arb.surfPol*c.ap_tot;
+figures.arb_exposed_surface = figure('Name', 'Airbrake exposed surface');
+c.dS = settings.arb.surfPol * c.ap_tot;
 plot(c.Tf_tot, c.dS);
 grid on;
 xlabel('Time [s]');
 ylabel('dS [m^2]');
 title('Airbrake exposed surface');
-exportgraphics(figures.arb_exposed_surface,'report_images\arb_exposed_surface.pdf')
+% exportgraphics(figures.arb_exposed_surface,'report_images\arb_exposed_surface.pdf')
 %% Trajectory
-figures.trajectory = figure('Name','Trajectory');
-plot3(c.Yf_tot(:,1),c.Yf_tot(:,2),-c.Yf_tot(:,3));
+figures.trajectory = figure('Name', 'Trajectory');
+plot3(c.Yf_tot(:, 1), c.Yf_tot(:, 2), -c.Yf_tot(:, 3));
 grid on;
 xlabel('x [m]');
 ylabel('y [m]');
 zlabel('z [m]');
 title('Trajectory');
-exportgraphics(figures.trajectory,'report_images\trajectory.pdf')
+% exportgraphics(figures.trajectory,'report_images\trajectory.pdf')
 
 %% Velocities w.r.t. time
-figures.velocities = figure('Name','Velocities');
-plot(c.Tf_tot,c.Yf_tot(:,4))
+figures.velocities = figure('Name', 'Velocities');
+plot(c.Tf_tot, c.Yf_tot(:, 4))
 hold on;
-plot(c.Tf_tot,c.Yf_tot(:,5))
-plot(c.Tf_tot,c.Yf_tot(:,6))
+plot(c.Tf_tot, c.Yf_tot(:, 5))
+plot(c.Tf_tot, c.Yf_tot(:, 6))
 grid on;
 xlabel('Time [s]');
 ylabel('Speed V [m/s]');
 title('Velocities');
-legend('Vx','Vy','Vz')
-exportgraphics(figures.velocities,'report_images\velocities.pdf')
+legend('Vx', 'Vy', 'Vz')
+% exportgraphics(figures.velocities,'report_images\velocities.pdf')
 
 %% Mach w.r.t. time
-figures.Mach_number = figure('Name','Velocities');
-[~,a,~,~] = atmosisa(c.Yf_tot(:,3));
-v_norm_vec = zeros(length(c.Yf_tot(:,1)),1);
-for i = 1:length(c.Yf_tot(:,1))
-v_norm_vec(i) = norm([c.Yf_tot(i,4),c.Yf_tot(i,5),c.Yf_tot(i,6)]);
+figures.Mach_number = figure('Name', 'Velocities');
+[~, a, ~, ~] = atmosisa(c.Yf_tot(:, 3));
+v_norm_vec = zeros(length(c.Yf_tot(:, 1)), 1);
+
+for i = 1:length(c.Yf_tot(:, 1))
+    v_norm_vec(i) = norm([c.Yf_tot(i, 4), c.Yf_tot(i, 5), c.Yf_tot(i, 6)]);
 end
-plot(c.Tf_tot,v_norm_vec./a)
+
+plot(c.Tf_tot, v_norm_vec ./ a)
 grid on;
 xlabel('Time t [s]');
 ylabel('Mach M(t) [-]');
 title('Velocities');
 legend('Mach')
-exportgraphics(figures.Mach_number,'report_images\Mach_number.pdf')
-
+% exportgraphics(figures.Mach_number,'report_images\Mach_number.pdf')
