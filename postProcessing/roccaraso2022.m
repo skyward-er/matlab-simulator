c6055c838f5c083d2126ba5ad301b747c2d779be
Massimiliano Restuccia <massimiliano.restuccia@skywarder.eu>
2022-09-26 11:55:05 +0200

    update

--- postProcessing/roccaraso2022.m
+++ postProcessing/roccaraso2022.m
@@ -12,38 +12,55 @@ addpath(genpath(currentPath));
 % Common Functions path
 addpath(genpath(commonFunctionsPath));
 addpath("..\simulator\")
-load("NAS.mat")
+load("roccaraso.mat")
 configSimulator; 
 configControl;
 configReferences;
 matlab_graphics; % thanks Massimiliano Restuccia
 
-%% air brakes motion
-nas = nas(1:738,:);
-nas(:,1) = nas(:,1)/1e6-nas(1,1)/1e6;
+for ii = 1:11
+    name_traj = strcat('referenceinterp',num2str(ii));
+    traj(ii).num = table2array(readtable(name_traj));
+end
 
-time_10 = nas(1,1):0.1:nas(end,1);
-nas  = interp1(nas(:,1),nas(:,1:end),time_10);
+%% air brakes motion
+time_nas = Main_roccaraso_flight.NAS.time;
+% time_nas = time_nas-Main_roccaraso_flight.events.time(1);
+time_10 = time_nas(1):0.1:time_nas(end);
+% nas  = interp1(nas(:,1),nas(:,1:end),time_10,'nearest');
 ap = 0;
 dt = 0.1;
 ap_save(1) = 0;
 ap_ref_save(1) =0;
+apogee = (Main_roccaraso_flight.events.time(4));
+liftoff = Main_roccaraso_flight.events.time(1);
 for ii = 2:length(time_10)
-
-    if nas(ii,1) >= 3.8
-
-        V_mod     = norm(nas(ii,5:7));
-        t_change_ref = nas(ii-1,1)+settings.servo.delay;
-        nas_state.time  =  nas(ii,1);
-        nas_state.z     = -nas(ii,4);
-        nas_state.vz    = -nas(ii,7);
+    [~,idx] = min(abs(time_nas(:,1)-time_10(ii)));
+    if time_nas(idx) >= liftoff+3.8 && time_nas(idx) <= apogee
+        V_mod     = norm(Main_roccaraso_flight.NAS.ned_vel(idx,:));
+        nas_state.time  =  time_nas(idx);
+        nas_state.z     = -Main_roccaraso_flight.NAS.ned_pos(idx,3);
+        nas_state.vz    = -Main_roccaraso_flight.NAS.ned_vel(idx,3);
         [alpha_degree, vz_setpoint, z_setpoint, contSettings] =control_PID(nas_state, V_mod, contSettings,settings);
-
         ap_ref(ii) =alpha_degree*pi/180;
     else
         ap_ref(ii) = 0;
     end
 end
+%%
+figure;
+yyaxis left
+plot(time_10,ap_ref);
+yyaxis right
+plot(time_nas(:,1),Main_roccaraso_flight.NAS.ned_vel);
+
+%%
+figure
+plot(Main_roccaraso_flight.IMU.time,Main_roccaraso_flight.IMU.acc(:,1));
+hold on
+plot(Main_roccaraso_flight.NAS.time,Main_roccaraso_flight.NAS.ned_vel(:,3));
+plot(time_10,ap_ref);
+
 
 %% airbrakes
 
@@ -52,9 +69,9 @@ air_servo = tf(1,[0.0479,1],'outputdelay',0.0412);
 alpha_real = lsim(air_servo,ap_ref,time_10);
 
 figure;
-plot(nas(:,1),ap_ref*180/pi,'DisplayName','Commanded');
+plot(time_10,ap_ref*180/pi,'DisplayName','Commanded');
 hold on
-plot(nas(:,1),alpha_real*180/pi,'DisplayName','Real');
+plot(time_10,alpha_real*180/pi,'DisplayName','Real');
 xlabel('Time [s]')
 ylabel('Air Brakes servo angle [deg]')
 legend()
@@ -63,3 +80,4 @@ legend()
 
 air_brakes.time       = time_10;
 air_brakes.servoangle = alpha_real';
+
