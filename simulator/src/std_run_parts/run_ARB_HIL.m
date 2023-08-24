3a9f7e8bc844604f8ec401cddafce1d94a434af5
Pier Francesco Bachini <pier.bachini@gmail.com>
2023-08-24 12:15:45 +0200

    Update Pitot data sent to obsw and update simulator flags and structs with data from obsw

--- simulator/src/std_run_parts/run_ARB_HIL.m
+++ simulator/src/std_run_parts/run_ARB_HIL.m
@@ -1,25 +1,30 @@
-function [airbrakes_opening, nas_timestamp, x_est_tot, ada_altitude, ada_verticalSpeed, ada_timestamp, estimated_mass, liftoff, burning_shutdown] = run_ARB_HIL(sensorData, flagsArray)
-    % alpha_degree  -> percentage opening of the airbrakes (% 0<=alpha_degree<=1)
-    % t_est_tot     -> timestamps NAS
-    % x_est_tot     -> vettore stato NAS
-    % xp_ada_tot    -> posizione ADA (mslAltitude)
-    % xv_ada_tot    -> velocità ADA (verticalSpeed)
-    % t_ada_tot     -> timestamps ADA
+function [airbrakes_opening, nas_timestamp, x_est_tot, ada_altitude, ada_verticalSpeed, ada_timestamp, estimated_mass, liftoff, burning_shutdown] = run_ARB_HIL(sensorData, sp, flagsArray)
 
-    % getting the fix and nSatellites
+    % airbrakes_opening     -> percentage opening of the airbrakes (% 0<=alpha_degree<=1)
+    % nas_timestamp         -> current NAS timestamp
+    % x_est_tot             -> current NAS state
+    % ada_altitude          -> current estimated ADA altitude (mslAltitude)
+    % ada_verticalSpeed     -> current estimated ADA vertical speed
+    % ada_timestamp         -> current ADA timestamp
+    % estimated_mass        -> mass estimated by the MEA obsw algorithm 
+    % liftoff               -> flag set to true if rocket is launched
+    % burning_shutdown      -> flag set to true if motor needs to be shutdown
+
+    % set gps fix and number of satellites
     sensorData.gps.fix = 3;
     sensorData.gps.nsat = 16;
 
     % sending sensor data over the serial port
-    sendDataOverSerial(sensorData, flagsArray);
+    sendDataOverSerial(sensorData, sp, flagsArray);
 
-    % waiting for the response of the MCU
+    % waiting for the response of the obsw
     [nas_timestamp, n,e,d, vn,ve,vd, qx,qy,qz,qw, bx,by,bz, ...
       ada_timestamp, ada_altitude, ada_verticalSpeed, ...
       airbrakes_opening, estimated_mass, liftoff, burning_shutdown ...
     ] = readControlOutputFromSerial();
     
-    x_est_tot = [n, e, d, vn, ve, vd, qx, qy, qz, qw, bx, by, bz];
+    % Create nas estimated state array
+    x_est_tot = [n, e, d, vn, ve, vd, qw, qx, qy, qz, bx, by, bz];
 
     % if the obsw sends an opening of -1 while the flag isLaunch is still
     % false, triggers the liftoff and the opening of aerobrake is set to 0
--- simulator/src/std_run_parts/std_hardwareInTheLoop.m
+++ simulator/src/std_run_parts/std_hardwareInTheLoop.m
@@ -3,21 +3,11 @@
 hardware in the loop script
 
 %}
-v_ned = quatrotate(quatconj(Yf(:, 10:13)), Yf(:, 4:6));
-% HIL HIL HIL HIL HIL HIL HIL HIL HIL HIL HIL
-% qua leggere da seriale e impostare i valori
-flagsArray = [flagFlight, settings.flagAscent, flagBurning, flagAeroBrakes, flagPara1, flagPara2];
 
-% NOTE: Why kalman is set here and not using data from obsw nas?
-if flagsArray(1)
-    sensorData.kalman.z    = -Yf(end, 3);
-    sensorData.kalman.vz   = Yf(end, 6);
-    sensorData.kalman.vMod = norm(Yf(end, 4:6));
-else
-    sensorData.kalman.z    = settings.z0;
-    sensorData.kalman.vz   = 0;
-    sensorData.kalman.vMod = 0;
-end
+%% Prepare data to be sent to and read from obsw
+v_ned = quatrotate(quatconj(Yf(:, 10:13)), Yf(:, 4:6));
+
+flagsArray = [flagFlight, settings.flagAscent, flagBurning, flagAeroBrakes, flagPara1, flagPara2];
 
 % Convert the gps position from meter to degrees
 [latitude, longitude, ~] = ned2geodetic( ...
@@ -31,23 +21,92 @@ sensorData.gps.longitude = longitude;
 % Add gravity acceleration
 sensorData.accelerometer.measures = sensorData.accelerometer.measures + (quat2rotm(Yf(1,11:14)) * [0;0;9.81])';
 
-ap_ref_old = ap_ref_new;
-[alpha_aperture, t_nas, x_est, xp_ada, xv_ada, t_ada, estimated_mass, liftoff, burning_shutdown] = run_ARB_HIL(sensorData, flagsArray);
+% Execute serial communication with obsw
+[alpha_aperture, t_nas, x_est, xp_ada, xv_ada, t_ada, estimated_mass, liftoff, burning_shutdown] = run_ARB_HIL(sensorData, sp, flagsArray);
+
+%% Update Airbrakes data
+
+if flagAeroBrakes && mach < settings.MachControl
+    if contSettings.flagFirstControlABK % set in
+        t_airbrakes = t0;
+        t_last_arb_control = t0;
+        idx_airbrakes = n_old+1;
+        contSettings.flagFirstControlABK = false;
+    else
+        % Update previous control value for airbrakes
+        ap_ref_old = ap_ref_new;
+        t_last_arb_control = Tf(end);
+        [~,settings.pitch,~] = quat2angle(settings.quat,'ZYX');
+        settings.quat = [x_est_tot(end, [10,7:9])];
+    end
+end
 
 ap_ref_new = alpha_aperture * settings.servo.maxAngle;  % alpha_aperture:
-settings.shutdown = burning_shutdown;
+
+%% Update NAS data
+
+if ~flagsArray(1)
+    sensorData.kalman.z    = settings.z0;
+    sensorData.kalman.vz   = 0;
+    sensorData.kalman.vMod = 0;
+else
+    sensorData.kalman.x  =  x_est_tot(end, 2);
+    sensorData.kalman.y  =  x_est_tot(end, 1);
+    sensorData.kalman.z  = -x_est_tot(end, 3);
+    sensorData.kalman.vx =  x_est_tot(end, 4);   % north
+    sensorData.kalman.vy =  x_est_tot(end, 5);   % east
+    sensorData.kalman.vz = -x_est_tot(end, 6);   % down
+end
+
 sensorData.kalman.time(iTimes) = Tf(end);
 sensorData.kalman.x_c = x_est;
-
 x_est_tot(c.n_est_old:c.n_est_old + size(sensorData.kalman.x_c(:,1),1)-1,:)  = sensorData.kalman.x_c(:,:); % NAS position output
-% vels_tot(c.n_est_old:c.n_est_old + size(vels(:,1),1)-1,:)  = vels(:,:); % NAS speed output
 t_est_tot(c.n_est_old:c.n_est_old + size(sensorData.kalman.x_c(:,1),1)-1)    = t_nas; % NAS time output
 c.n_est_old = c.n_est_old + size(sensorData.kalman.x_c,1);
 
+Q = x_est(end, 7:10);
+vels = quatrotate(quatconj(Q), Yf(end, 4:6));
+vels_tot(c.n_est_old:c.n_est_old + size(vels(:,1),1)-1,:) = vels(:,:); % NAS speed output
+
+%% Update ADA data
+% Missing:
+% settings.ada.t_ada as the time where the predicted apogee by ada is
+
 xp_ada_tot(c.n_ada_old:c.n_ada_old + size(xp_ada(:,1),1) -1,:)  = xp_ada(1:end,:);
-xv_ada_tot(c.n_ada_old:c.n_ada_old + size(xv_ada(:,1),1)-1,:)  = xv_ada(1:end,:);
-t_ada_tot(c.n_ada_old:c.n_ada_old + size(xp_ada(:,1),1)-1)     = t_ada;
+xv_ada_tot(c.n_ada_old:c.n_ada_old + size(xv_ada(:,1),1)-1,:)   = xv_ada(1:end,:);
+t_ada_tot(c.n_ada_old:c.n_ada_old + size(xp_ada(:,1),1)-1)      = t_ada;
 c.n_ada_old = c.n_ada_old + size(xp_ada,1);
+
+%% Update Mass estimation data
+
+settings.shutdown = burning_shutdown;
+estimated_pressure = NaN;
+
+if settings.shutdown && Tf(end) < settings.tb
+    t_shutdown = Tf(end);
+    settings.expShutdown = 1;
+    settings.timeEngineCut = t_shutdown;
+    settings.expTimeEngineCut = t_shutdown;
+    settings.IengineCut = Yf(end,14:16);
+    settings.expMengineCut = m - settings.ms;
+    settings.shutdown = 1;
+    settings = settingsEngineCut(settings);
+    settings.quatCut = [x_est_tot(end, 8:10) x_est_tot(end, 7)];
+    [~,settings.pitchCut,~] = quat2angle(settings.quatCut,'ZYX');
+elseif ~settings.shutdown && Tf(end) >= settings.tb
+    t_shutdown = settings.tb;
+    settings.expShutdown = 1;
+    settings.timeEngineCut = t_shutdown;
+    settings.expTimeEngineCut = t_shutdown;
+    settings.IengineCut = Yf(end,14:16);
+    settings.expMengineCut = m - settings.ms;
+    settings.shutdown = 1;
+    settings = settingsEngineCut(settings);
+    settings.quatCut = [x_est_tot(end, 8:10) x_est_tot(end, 7)];
+    [~,settings.pitchCut,~] = quat2angle(settings.quatCut,'ZYX');
+end
+
+%% Update Sensor fault data
 
 % Temporary code as sensor fault is not yet implemented on obsw hil
 faulty_sensors = [];
