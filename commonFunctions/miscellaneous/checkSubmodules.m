070ca08603526dc4f5fffbad620453f8a3449f11
Marco Marchesi <marco.marchesi@skywarder.eu>
2023-08-27 10:59:18 +0200

    update on automatic msa-toolkit pull

--- commonFunctions/miscellaneous/checkSubmodules.m
+++ commonFunctions/miscellaneous/checkSubmodules.m
@@ -1,5 +1,5 @@
 msaToolkitURL = 'https://git.skywarder.eu/afd/msa/msa-toolkit';
-localRepoPath = '../data/msa-toolkit';
+localRepoPath_msaToolkit = '../data/msa-toolkit';
 % status = checkLastCommit(msaToolkitURL, localRepoPath, pwd);
 % submoduleAdvice(status, msaToolkitURL, localRepoPath, pwd);
 if ~exist('../data/msa-toolkit/data',"dir")
@@ -8,21 +8,21 @@ if ~exist('../data/msa-toolkit/data',"dir")
         cloneType = input('Which type of clone do you want to use? (SSH/HTTP) ','s');
         branchName = input('Which branch do you want to checkout? ','s');
         if cloneType =="SSH" || cloneType == "ssh"
-            system('git clone "git@git.skywarder.eu:afd/msa/msa-toolkit.git"')
+            system(['git clone "git@git.skywarder.eu:afd/msa/msa-toolkit.git" ',localRepoPath_msaToolkit])
         elseif cloneType == "HTTP" || cloneType == "http"
-            system('git clone "https://git.skywarder.eu/afd/msa/msa-toolkit.git" ../data/msa-toolkit')
+            system(['git clone "https://git.skywarder.eu/afd/msa/msa-toolkit.git" ',localRepoPath_msaToolkit])
         else
             fprintf('\n WARNING! Input not valid: aborting')
         return
         end
-        cd('../data/msa-toolkit/')
+        cd(localRepoPath_msaToolkit)
         system('git fetch')
         system(['git checkout ',branchName])
         cd(currentPath)
     end
     fprintf('\n msa-toolkit installed.\nCurrent branch: %s',branchName)
 else
-    cd('../data/msa-toolkit/')
+    cd(localRepoPath_msaToolkit)
     system('git fetch')
     system('git pull')
     cd(currentPath)
@@ -30,16 +30,17 @@ end
 
 if ~exist('..\commonFunctions\graphics\general-utilities\','dir')
     % clone repo
+    localRepoPath_generalUtilities = '..\commonFunctions\graphics\general-utilities\';
     answer = input('WARNING! You don''have the exportStandardizedFigure installed. Do you want to install it? (y/n)','s');
     if answer == "y" || answer == "yes"
         
         cloneType = input('Which type of clone do you want to use? (SSH/HTTP) ','s');
         if cloneType =="SSH" || cloneType == "ssh"
             mkdir('..\commonFunctions\graphics\general-utilities\')    
-            system('git clone "git@git.skywarder.eu:skyward/general-utilities.git" ..\commonFunctions\graphics\')
+            system(['git clone "git@git.skywarder.eu:skyward/general-utilities.git" ',localRepoPath_generalUtilities])
         elseif cloneType == "HTTP" || cloneType == "http"
             mkdir('..\commonFunctions\graphics\general-utilities\')
-            system('git clone "https://git.skywarder.eu/skyward/general-utilities.git" ..\commonFunctions\graphics\')
+            system(['git clone "https://git.skywarder.eu/skyward/general-utilities.git" ',localRepoPath_generalUtilities])
         else
             fprintf('\n WARNING! Input not valid: aborting')
         return
@@ -47,7 +48,7 @@ if ~exist('..\commonFunctions\graphics\general-utilities\','dir')
     end
 else 
     % update (pull)
-    cd('..\commonFunctions\graphics\general-utilities\')
+    cd(localRepoPath_generalUtilities)
     system('git fetch')
     system('git pull')
     cd(currentPath)
