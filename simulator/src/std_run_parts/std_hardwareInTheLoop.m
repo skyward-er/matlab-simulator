662de0da78600e8ed3dfe7e058f4bd98fb0cc9a5
Emilio Corigliano <emilio.corigliano@skywarder.eu>
2023-09-26 18:46:44 +0200

    [HIL] Modified std_hardwareInTheLoop
    - fixed correction of accelerometer measures with gravity
    - fixed expulsion events triggered by the obsw
    - added parafoil options
    - initialized all NAS position and speed to zero

--- simulator/src/std_run_parts/std_hardwareInTheLoop.m
+++ simulator/src/std_run_parts/std_hardwareInTheLoop.m
@@ -34,7 +34,7 @@ flagsArray = [isLaunch, settings.flagAscent, flagBurning, flagAeroBrakes, flagPa
 
 % Add gravity acceleration only when still on ramp
 if ~flagFlight
-    sensorData.accelerometer.measures = sensorData.accelerometer.measures + (quat2rotm(Yf(end,10:13)) * [0;0;9.81])';
+    sensorData.accelerometer.measures = sensorData.accelerometer.measures + (quat2dcm(Yf(end,10:13)) * [0;0;-9.81])';
 end
 
 % extract frequencies from settings struct
@@ -55,22 +55,48 @@ flagAeroBrakes = hilData.flagsArray(4);
 flagPara1 = hilData.flagsArray(5);
 flagPara2 = hilData.flagsArray(6);
 
+if(flagPara1 >0)
+    eventExpulsion = true;
+end
+
+if(flagPara2 >0)
+    eventExpulsion2 = true;
+end
+
+
 disp("HIL flight: " + hilData.flagsArray(1) + ", ascent: " + hilData.flagsArray(2) + ...
     ", burning: " + hilData.flagsArray(3) + ", airbrakes: " + hilData.flagsArray(4) + ...
     ", para1: " + hilData.flagsArray(5) + ", para2: " + hilData.flagsArray(6));
 
 %% Update ADA data
+if ~settings.parafoil
+    sensorData.ada.xp = hilData.ada.mslAltitude;
+    sensorData.ada.xv = hilData.ada.verticalSpeed;
+    settings.ada.flag_apo = hilData.ada.apogeeDetected;
 
-sensorData.ada.xp = hilData.ada.mslAltitude;
-sensorData.ada.xv = hilData.ada.verticalSpeed;
-settings.ada.flag_apo = hilData.ada.apogeeDetected;
+    sensorTot.ada.xp(sensorTot.ada.n_old:sensorTot.ada.n_old + size(sensorData.ada.xp(:,1),1) -1,:)  = sensorData.ada.xp(1:end,:);
+    sensorTot.ada.xv(sensorTot.ada.n_old:sensorTot.ada.n_old + size(sensorData.ada.xv(:,1),1)-1,:)  = sensorData.ada.xv(1:end,:);
+    sensorTot.ada.t(sensorTot.ada.n_old:sensorTot.ada.n_old + size(sensorData.ada.xp(:,1),1)-1)     = sensorData.barometer.time(end);
+    sensorTot.ada.n_old = sensorTot.ada.n_old + size(sensorData.ada.xp,1);
+    settings.baro_old = sensorData.barometer.time(end);
+else
+    if iTimes>3
+        if settings.flagADA
+            ada_prev  =   sensorTot.ada.xp(end,:);
+            Pada_prev =   sensorData.ada.P(:,:,end);
+        end
+    end
 
-sensorTot.ada.xp(sensorTot.ada.n_old:sensorTot.ada.n_old + size(sensorData.ada.xp(:,1),1) -1,:)  = sensorData.ada.xp(1:end,:);
-sensorTot.ada.xv(sensorTot.ada.n_old:sensorTot.ada.n_old + size(sensorData.ada.xv(:,1),1)-1,:)  = sensorData.ada.xv(1:end,:);
-sensorTot.ada.t(sensorTot.ada.n_old:sensorTot.ada.n_old + size(sensorData.ada.xp(:,1),1)-1)     = sensorData.barometer.time(end);
-sensorTot.ada.n_old = sensorTot.ada.n_old + size(sensorData.ada.xp,1);
-settings.baro_old = sensorData.barometer.time(end);
+    if settings.flagADA && settings.dataNoise && sensorData.barometer.time(1) >= settings.baro_old
+        [sensorData.ada.xp, sensorData.ada.xv, sensorData.ada.P, settings.ada]   =  run_ADA(ada_prev, Pada_prev, sensorData.barometer.measures, sensorData.barometer.time, settings);
 
+        sensorTot.ada.xp(sensorTot.ada.n_old:sensorTot.ada.n_old + size(sensorData.ada.xp(:,1),1) -1,:)  = sensorData.ada.xp(1:end,:);
+        sensorTot.ada.xv(sensorTot.ada.n_old:sensorTot.ada.n_old + size(sensorData.ada.xv(:,1),1)-1,:)  = sensorData.ada.xv(1:end,:);
+        sensorTot.ada.time(sensorTot.ada.n_old:sensorTot.ada.n_old + size(sensorData.ada.xp(:,1),1)-1)     = sensorData.barometer.time;
+        sensorTot.ada.n_old = sensorTot.ada.n_old + size(sensorData.ada.xp,1);
+        settings.baro_old = sensorData.barometer.time(end);
+    end
+end
 
 %% Update NAS data
 
@@ -82,7 +108,11 @@ sensorTot.nas.time(sensorTot.nas.n_old:sensorTot.nas.n_old + size(sensorData.nas
 sensorTot.nas.n_old = sensorTot.nas.n_old + size(sensorData.nas.states,1);
 
 if ~flagFlight
+    sensorData.kalman.x    = 0;
+    sensorData.kalman.y    = 0;
     sensorData.kalman.z    = 0;
+    sensorData.kalman.vx   = 0;
+    sensorData.kalman.vy   = 0;
     sensorData.kalman.vz   = 0;
     sensorData.kalman.vMod = 0;
 else
@@ -95,63 +125,209 @@ else
 end
 
 %% Update Mass estimation data
+if ~settings.parafoil
+    lastShutdown = settings.shutdown;
+    settings.shutdown = not(flagBurning);
 
-lastShutdown = settings.shutdown;
-settings.shutdown = not(flagBurning);
+    if ~settings.shutdown
+        sensorData.mea.estimated_mass = hilData.mea.estimatedMass;
+        sensorData.mea.estimated_pressure = hilData.mea.correctedPressure;
+        sensorData.mea.predicted_apogee = hilData.mea.estimatedApogee;
 
+        m = sensorData.mea.estimated_mass(end);
 
-if ~settings.shutdown
-    sensorData.mea.estimated_mass = hilData.mea.estimatedMass;
-    sensorData.mea.estimated_pressure = hilData.mea.correctedPressure;
-    sensorData.mea.predicted_apogee = hilData.mea.estimatedApogee;
+        sensorTot.mea.pressure(iTimes) = sensorData.mea.estimated_pressure;
+        sensorTot.mea.mass(iTimes) = sensorData.mea.estimated_mass;
+        sensorTot.mea.prediction(iTimes) = sensorData.mea.predicted_apogee;
+        sensorTot.mea.time(iTimes) = t1;
+    end
 
-    m = sensorData.mea.estimated_mass(end);
+    if settings.shutdown && not(lastShutdown) && flagFlight     % Need to check if this happens only once or the condition can be met multiple times
+        t_shutdown = Tf(end);
+        settings.expShutdown = 1;
+        settings.timeEngineCut = t_shutdown;
+        settings.expTimeEngineCut = t_shutdown;
+        settings.expMengineCut = m - settings.ms;
+        settings.shutdown = 1;
+        settings = settingsEngineCut(settings);
+        settings.quatCut = [sensorTot.nas.states(end,10) sensorTot.nas.states(end, 7:9)]; % why do we take the nas ones and not the simulation ones?
+        [~,settings.pitchCut,~] = quat2angle(settings.quatCut,'ZYX');
+        sensorTot.mea.t_shutdown = t_shutdown; % to pass the value out of the std_run to the structOut
+    elseif ~settings.shutdown && Tf(end) >= settings.tb
+        t_shutdown = settings.tb;
+        settings.expShutdown = 1;
+        settings.timeEngineCut = t_shutdown;
+        settings.expTimeEngineCut = t_shutdown;
+        settings.expMengineCut = m - settings.ms;
+        settings.shutdown = 1;
+        settings = settingsEngineCut(settings);
+        settings.quatCut = [sensorTot.nas.states(end,10) sensorTot.nas.states(end, 7:9)]; % why do we take the nas ones and not the simulation ones?
+        [~,settings.pitchCut,~] = quat2angle(settings.quatCut,'ZYX');
+        sensorTot.mea.t_shutdown = t_shutdown; % to pass the value out of the std_run to the structOut
+    end
+else
+    if contains(settings.mission,'_2023')
+        if Tf(end) <= settings.tb+0.5 &&...
+                (strcmp(contSettings.algorithm,'engine') || strcmp(contSettings.algorithm,'complete'))
 
-    sensorTot.mea.pressure(iTimes) = sensorData.mea.estimated_pressure;
-    sensorTot.mea.mass(iTimes) = sensorData.mea.estimated_mass;
-    sensorTot.mea.prediction(iTimes) = sensorData.mea.predicted_apogee;
-    sensorTot.mea.time(iTimes) = t1;
-end
+            if isnan(sensorTot.comb_chamber.measures(end))
+                sensorTot.comb_chamber.measures(end) = 0;
+            end
+            if ~settings.shutdown
+                [t_shutdown,settings,contSettings,sensorData] =run_MTR_SIM (contSettings,sensorData,settings,sensorTot,Tf);
+                m = sensorData.mea.estimated_mass(end);
+                sensorTot.mea.pressure(iTimes) = sensorData.mea.estimated_pressure;
+                sensorTot.mea.mass(iTimes) = sensorData.mea.estimated_mass;
+                sensorTot.mea.prediction(iTimes) = sensorData.mea.predicted_apogee;
+                sensorTot.mea.t_shutdown = t_shutdown;
+                sensorTot.mea.time(iTimes) = t1;
+            end
 
-if settings.shutdown && not(lastShutdown) && flagFlight     % Need to check if this happens only once or the condition can be met multiple times
-    t_shutdown = Tf(end);
-    settings.expShutdown = 1;
-    settings.timeEngineCut = t_shutdown;
-    settings.expTimeEngineCut = t_shutdown;
-    settings.expMengineCut = m - settings.ms;
-    settings.shutdown = 1;
-    settings = settingsEngineCut(settings);
-    settings.quatCut = [sensorTot.nas.states(end,10) sensorTot.nas.states(end, 7:9)]; % why do we take the nas ones and not the simulation ones?
-    [~,settings.pitchCut,~] = quat2angle(settings.quatCut,'ZYX');
-    sensorTot.mea.t_shutdown = t_shutdown; % to pass the value out of the std_run to the structOut
-elseif ~settings.shutdown && Tf(end) >= settings.tb
-    t_shutdown = settings.tb;
-    settings.expShutdown = 1;
-    settings.timeEngineCut = t_shutdown;
-    settings.expTimeEngineCut = t_shutdown;
-    settings.expMengineCut = m - settings.ms;
-    settings.shutdown = 1;
-    settings = settingsEngineCut(settings);
-    settings.quatCut = [sensorTot.nas.states(end,10) sensorTot.nas.states(end, 7:9)]; % why do we take the nas ones and not the simulation ones?
-    [~,settings.pitchCut,~] = quat2angle(settings.quatCut,'ZYX');
-    sensorTot.mea.t_shutdown = t_shutdown; % to pass the value out of the std_run to the structOut
+            if ~settings.shutdown && Tf(end) >= settings.tb
+                t_shutdown = settings.tb;
+                settings.expShutdown = 1;
+                settings.timeEngineCut = t_shutdown;
+                settings.expTimeEngineCut = t_shutdown;
+                settings.expMengineCut = m - settings.ms;
+                settings.shutdown = 1;
+                settings = settingsEngineCut(settings);
+                settings.quatCut = [sensorTot.nas.states(end,10) sensorTot.nas.states(end, 7:9)]; % why do we take the nas ones and not the simulation ones?
+                [~,settings.pitchCut,~] = quat2angle(settings.quatCut,'ZYX');
+                sensorTot.mea.t_shutdown = t_shutdown; % to pass the value out of the std_run to the structOut
+            end
+        elseif ~(strcmp(contSettings.algorithm,'engine') || strcmp(contSettings.algorithm,'complete')) && Tf(end) > settings.tb
+            settings.shutdown = 1;
+            settings.expShutdown = 1;
+            settings.timeEngineCut = settings.tb;
+            settings.expTimeEngineCut = settings.tb;
+        end
+    else
+        if t0 > settings.motor.expTime(end)
+            settings.expShutdown = 1;
+        end
+    end
+
 end
 
 %% Update Airbrakes data
+if ~settings.parafoil
+    if flagAeroBrakes && mach < settings.MachControl
+        if contSettings.flagFirstControlABK % set in
+            t_airbrakes = t0;
+            t_last_arb_control = t0;
+            idx_airbrakes = n_old+1;
+            contSettings.flagFirstControlABK = false;
+        else
+            % Update previous control value for airbrakes
+            ap_ref_old = ap_ref_new;
+            t_last_arb_control = Tf(end);
+            settings.quat = [sensorTot.nas.states(end, [10,7:9])];
+            [~,settings.pitch,~] = quat2angle(settings.quat,'ZYX');
+        end
+    end
 
-if flagAeroBrakes && mach < settings.MachControl
-    if contSettings.flagFirstControlABK % set in
-        t_airbrakes = t0;
-        t_last_arb_control = t0;
-        idx_airbrakes = n_old+1;
-        contSettings.flagFirstControlABK = false;
+    ap_ref_new = hilData.abk.airbrakes_opening * settings.servo.maxAngle;  % alpha_aperture:
+else
+    if contains(settings.mission,'_2023')
+        if flagAeroBrakes && mach < settings.MachControl && settings.flagNAS && settings.control...
+                && ~(strcmp(contSettings.algorithm,'NoControl') || strcmp(contSettings.algorithm,'engine') ) ...
+                && Tf(end) > settings.expTimeEngineCut + 0.5
+
+            if str2double(settings.mission(end)) > 2 % only for mission after october 2022
+
+                if contSettings.traj_choice == 1 && settings.expShutdown
+                    if ~(strcmp(contSettings.algorithm,'engine') || strcmp(contSettings.algorithm,'complete'))
+                        m = settings.ms;
+                    else
+                        m = sensorData.mea.estimated_mass(end);
+                    end
+                    contSettings = trajectoryChoice_mass(m,contSettings);
+                end
+            end
+
+            if contSettings.flagFirstControlABK % set in
+
+                t_airbrakes = t0;
+                t_last_arb_control = t0;
+                idx_airbrakes = n_old+1;
+
+            end
+            if t1-t_last_arb_control >= 1/settings.frequencies.arbFrequency - 1e-5 || t_last_arb_control == t_airbrakes
+                t_last_arb_control = Tf(end);
+                ap_ref_old = ap_ref_new;
+                settings.quat = [sensorTot.nas.states(end, [10,7:9])];
+                [~,settings.pitch,~] = quat2angle(settings.quat,'ZYX');
+                [ap_ref_new,contSettings] = run_ARB_SIM(sensorData,settings,contSettings,ap_ref_old); % "simulated" airbrakes because otherwise are run by the HIL.
+            end
+        else
+            ap_ref_new = 0;
+        end
     else
-        % Update previous control value for airbrakes
-        ap_ref_old = ap_ref_new;
-        t_last_arb_control = Tf(end);
-        settings.quat = [sensorTot.nas.states(end, [10,7:9])];
-        [~,settings.pitch,~] = quat2angle(settings.quat,'ZYX');
+        if flagAeroBrakes && mach < settings.MachControl && settings.flagNAS && settings.control
+
+            if contSettings.flagFirstControlABK
+
+                t_airbrakes = t0;
+                t_last_arb_control = t0;
+                idx_airbrakes = n_old+1;
+
+            end
+            if t1-t_last_arb_control >= 1/settings.frequencies.arbFrequency - 1e-6 || t_last_arb_control == t_airbrakes
+                t_last_arb_control = t1(end);
+                ap_ref_old = ap_ref_new;
+                settings.quat = [sensorTot.nas.states(end, [10,7:9])];
+                [~,settings.pitch,~] = quat2angle(settings.quat,'ZYX');
+                [ap_ref_new,contSettings] = run_ARB_SIM(sensorData,settings,contSettings,ap_ref_old); % "simulated" airbrakes because otherwise are run by the HIL.
+
+            end
+
+        else
+            ap_ref_new = 0;
+        end
+    end
+    ap_ref_new = 1.1717;
+end
+
+%% PARAFOIL
+if ~settings.flagAscent && settings.parafoil 
+    if flagPara2
+        if contSettings.flagFirstControlPRF % set in
+                t_parafoil = t1;
+                t_last_prf_control = t1;
+                idx_parafoil = n_old+1;
+                contSettings.flagFirstControlPRF = false;
+                if contSettings.payload.guidance_alg == "t-approach"
+                    pos_est = sensorData.nas.states(end,1:3);
+                    pos_est(3) = -pos_est(3)-settings.z0;
+                    [contSettings.payload.EMC,contSettings.payload.M1,contSettings.payload.M2] = setEMCpoints(pos_est,settings.payload.target,contSettings.payload);
+                end
+        end
+        if contSettings.payload.flagWES
+%                 if t1 < t_parafoil + contSettings.payload.guidance_start
+%                     contSettings.WES.state = 1;
+%                 else
+%                     contSettings.WES.state = 2;
+%                 end
+                wind_est = hilData.wes;
+                sensorTot.wes.measure(iTimes,:) = wind_est; 
+                sensorTot.wes.time(iTimes) = t1; 
+        else
+                wind_est = [0,0];
+        end
+    
+%         if t1-t_last_prf_control >= 1/settings.frequencies.prfFrequency - 1e-6 || t_last_prf_control == t_parafoil
+%                 deltaA_ref_old = deltaA_ref_new;
+%                 t_last_prf_control = t1;
+%                 pos_est = sensorData.nas.states(end,1:3);
+%                 pos_est(3) = -pos_est(3)-settings.z0;
+%                 
+%                 [deltaA_ref_new,contSettings] = run_parafoilGuidance(pos_est, sensorData.nas.states(end,4:5), wind_est, settings.payload.target, contSettings);
+%         end
+
+        deltaA_ref_old = deltaA_ref_new;
+        deltaA_ref_new = hilData.gnc.deltaA;
+
+        
     end
 end
-    
-ap_ref_new = hilData.abk.airbrakes_opening * settings.servo.maxAngle;  % alpha_aperture:
+   
