901039d7754fda06175816dfac3f6ba8b708ea54
Emilio Corigliano <emilio.corigliano@skywarder.eu>
2023-09-26 18:49:04 +0200

    Added gemini roccaraso september 2023 reference configs

--- simulator/configs/configReferences.m
+++ simulator/configs/configReferences.m
@@ -89,6 +89,19 @@ switch settings.mission
                 end
             end
         end
+    case 'Gemini_Roccaraso_September_2023'
+        load("Trajectories.mat")
+        for i = 1:size(trajectories_saving,1)
+            for j = 1:size(trajectories_saving,2)
+                reference.vz_ref{i,j} = trajectories_saving{i,j}.VZ_ref;
+                reference.vy_ref{i,j} = trajectories_saving{i,j}.VY_ref;
+                reference.vx_ref{i,j} = trajectories_saving{i,j}.VX_ref;
+                reference.altitude_ref{i,j} = trajectories_saving{i,j}.Z_ref;
+                for k = 1:length(reference.vz_ref{i,1})
+                    reference.Vnorm_ref{i,j}(k) = norm([reference.vz_ref{i,j}(k) reference.vx_ref{i,j}(k) reference.vy_ref{i,j}(k)]);
+                end
+            end
+        end
 end
 
 
