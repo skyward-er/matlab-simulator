fdbfe8d7ffc52353835da1063fb3c807aabaf638
Pier Francesco A. Bachini <pierfrancesco.bachini@skywarder.eu>
2023-11-04 11:29:07 +0100

    Fix how ADA data is saved to make it compatible with std_plots

--- simulator/src/std_run_parts/std_hardwareInTheLoop.m
+++ simulator/src/std_run_parts/std_hardwareInTheLoop.m
@@ -74,9 +74,9 @@ if ~settings.parafoil
     sensorData.ada.xv = hilData.ada.verticalSpeed;
     settings.ada.flag_apo = hilData.ada.apogeeDetected;
 
-    sensorTot.ada.xp(sensorTot.ada.n_old:sensorTot.ada.n_old + size(sensorData.ada.xp(:,1),1) -1,:)  = sensorData.ada.xp(1:end,:);
-    sensorTot.ada.xv(sensorTot.ada.n_old:sensorTot.ada.n_old + size(sensorData.ada.xv(:,1),1)-1,:)  = sensorData.ada.xv(1:end,:);
-    sensorTot.ada.t(sensorTot.ada.n_old:sensorTot.ada.n_old + size(sensorData.ada.xp(:,1),1)-1)     = sensorData.barometer.time(end);
+    sensorTot.ada.xv(sensorTot.ada.n_old:sensorTot.ada.n_old + size(sensorData.ada.xp(:,1),1) -1,1)  = sensorData.ada.xp(1:end,:);
+    sensorTot.ada.xv(sensorTot.ada.n_old:sensorTot.ada.n_old + size(sensorData.ada.xv(:,1),1)-1,2)  = sensorData.ada.xv(1:end,:);
+    sensorTot.ada.time(sensorTot.ada.n_old:sensorTot.ada.n_old + size(sensorData.ada.xp(:,1),1)-1)     = sensorData.barometer.time(end);
     sensorTot.ada.n_old = sensorTot.ada.n_old + size(sensorData.ada.xp,1);
     settings.baro_old = sensorData.barometer.time(end);
 else
