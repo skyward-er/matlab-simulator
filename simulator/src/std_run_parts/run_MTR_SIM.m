3acf08b56f18b26362090646b95e775c4081af2c
Pier Francesco A. Bachini <pierfrancesco.bachini@skywarder.eu>
2024-02-24 18:04:20 +0100

    [MTR] Changed magic number to shutdownValveDelay

--- simulator/src/std_run_parts/run_MTR_SIM.m
+++ simulator/src/std_run_parts/run_MTR_SIM.m
@@ -15,7 +15,7 @@ if sensorTot.mea.prediction(end) >= settings.mea.z_shutdown
         if ~settings.expShutdown && settings.mea.counter_shutdown > contSettings.N_prediction_threshold && T1 > settings.mea.t_lower_shadowmode% threshold set in configControl
             settings.expShutdown = true;
             settings.t_shutdown = T1;
-            settings.timeEngineCut = settings.t_shutdown + 0.3;
+            settings.timeEngineCut = settings.t_shutdown + settings.shutdownValveDelay;
             settings.expTimeEngineCut = settings.t_shutdown;
         end
         if ~settings.expShutdown && T1 >= settings.mea.t_higher_shadowmode
