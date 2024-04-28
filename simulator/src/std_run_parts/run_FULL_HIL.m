488a54132529973ff5b9882a7624a0bb13c604b1
Emilio Corigliano <emilio.corigliano@skywarder.eu>
2024-04-28 13:20:01 +0200

    [HIL] Now the communication for full hil is nominal, sending and receiving from all 3 electronics

--- simulator/src/std_run_parts/run_FULL_HIL.m
+++ simulator/src/std_run_parts/run_FULL_HIL.m
@@ -101,83 +101,83 @@ OUTPUTS:
 
     
     % sending sensor data over the serial port
-     serialbridge('Write','main', arrayToBeSent.main);
-     serialbridge('Write','payload', arrayToBeSent.payload);
-     serialbridge('Write','motor', arrayToBeSent.motor);  
+    serialbridge('Write','main', arrayToBeSent.main);
+    serialbridge('Write','payload', arrayToBeSent.payload);
+    serialbridge('Write','motor', arrayToBeSent.motor);  
 
 
     % waiting for the response of the obsw
-    % Receive data from serial comunication for main 
-        obswVals = serialbridge('Read','main', 32);
+    % Receive data from serial comunication for main
+    obswVals = serialbridge('Read','main', 32);
 
-        actuatorData.ada.mslAltitude = obswVals(1);
-        actuatorData.ada.aglAltitude = obswVals(2);
-        actuatorData.ada.verticalSpeed = obswVals(3);
-        actuatorData.ada.apogeeDetected = obswVals(4);
-        actuatorData.ada.updating = obswVals(5);
-        % actuatorData.nas.n = obswVals(6);
-        % actuatorData.nas.e = obswVals(7);
-        % actuatorData.nas.d = obswVals(8);
-        % actuatorData.nas.vn = obswVals(9);
-        % actuatorData.nas.ve = obswVals(10);
-        % actuatorData.nas.vd = obswVals(11);
-        % actuatorData.nas.qx = obswVals(12);
-        % actuatorData.nas.qy = obswVals(13);
-        % actuatorData.nas.qz = obswVals(14);
-        % actuatorData.nas.qw = obswVals(15);
-        % actuatorData.nas.updating = obswVals(16);
-        actuatorData.abk.updating = obswVals(17);
-        actuatorData.mea.correctedPressure = obswVals(18);
-        actuatorData.mea.estimatedMass = obswVals(19);
-        actuatorData.mea.estimatedApogee = obswVals(20);
-        actuatorData.mea.updating = obswVals(21);
-        actuatorData.actuators.airbrakesPercentage = obswVals(22);
-        actuatorData.actuators.expulsionPercentage = obswVals(23);
-        actuatorData.actuators.mainValvePercentage = obswVals(24);
-        actuatorData.actuators.ventingValvePercentage = obswVals(25);
-        actuatorData.actuators.mainCutterState = obswVals(26);
-        actuatorData.flags.flag_flight = logical(obswVals(27));
-        actuatorData.flags.flag_ascent = logical(obswVals(28));
-        actuatorData.flags.flag_burning = logical(obswVals(29));
-        actuatorData.flags.flag_airbrakes = logical(obswVals(30));
-        actuatorData.flags.flag_para1 = logical(obswVals(31));
-        % actuatorData.flags.flag_para2 = logical(obswVals(32));
+    actuatorData.ada.mslAltitude = obswVals(1);
+    actuatorData.ada.aglAltitude = obswVals(2);
+    actuatorData.ada.verticalSpeed = obswVals(3);
+    actuatorData.ada.apogeeDetected = obswVals(4);
+    actuatorData.ada.updating = obswVals(5);
+    % actuatorData.nas.n = obswVals(6);
+    % actuatorData.nas.e = obswVals(7);
+    % actuatorData.nas.d = obswVals(8);
+    % actuatorData.nas.vn = obswVals(9);
+    % actuatorData.nas.ve = obswVals(10);
+    % actuatorData.nas.vd = obswVals(11);
+    % actuatorData.nas.qx = obswVals(12);
+    % actuatorData.nas.qy = obswVals(13);
+    % actuatorData.nas.qz = obswVals(14);
+    % actuatorData.nas.qw = obswVals(15);
+    % actuatorData.nas.updating = obswVals(16);
+    actuatorData.abk.updating = obswVals(17);
+    actuatorData.mea.correctedPressure = obswVals(18);
+    actuatorData.mea.estimatedMass = obswVals(19);
+    actuatorData.mea.estimatedApogee = obswVals(20);
+    actuatorData.mea.updating = obswVals(21);
+    actuatorData.actuators.airbrakesPercentage = obswVals(22);
+    actuatorData.actuators.expulsionPercentage = obswVals(23);
+    % actuatorData.actuators.mainValvePercentage = obswVals(24);
+    % actuatorData.actuators.ventingValvePercentage = obswVals(25);
+    actuatorData.actuators.mainCutterState = obswVals(26);
+    actuatorData.flags.flag_flight = logical(obswVals(27));
+    actuatorData.flags.flag_ascent = logical(obswVals(28));
+    actuatorData.flags.flag_burning = logical(obswVals(29));
+    actuatorData.flags.flag_airbrakes = logical(obswVals(30));
+    actuatorData.flags.flag_para1 = logical(obswVals(31));
+    % actuatorData.flags.flag_para2 = logical(obswVals(32));
 
     % Receive data from serial comunication for payload
-        obswVals = serialbridge('Read','payload', 26);
+    obswVals = serialbridge('Read','payload', 26);
 
-        actuatorData.nas.n = obswVals(1);
-        actuatorData.nas.e = obswVals(2);
-        actuatorData.nas.d = obswVals(3);
-        actuatorData.nas.vn = obswVals(4);
-        actuatorData.nas.ve = obswVals(5);
-        actuatorData.nas.vd = obswVals(6);
-        actuatorData.nas.qx = obswVals(7);
-        actuatorData.nas.qy = obswVals(8);
-        actuatorData.nas.qz = obswVals(9);
-        actuatorData.nas.qw = obswVals(10);
-        actuatorData.nas.updating = obswVals(11);
-        actuatorData.actuators.parafoilLeftPercentage = obswVals(12);
-        actuatorData.actuators.parafoilRightPercentage = obswVals(13);
-        actuatorData.actuators.cutterState = obswVals(14);
-        actuatorData.wes.windX = obswVals(15);
-        actuatorData.wes.windY = obswVals(16);
-        actuatorData.guidance.psiRef = obswVals(17);
-        actuatorData.guidance.deltaA = obswVals(18);
-        actuatorData.guidance.currentTargetN = obswVals(19);
-        actuatorData.guidance.currentTargetE = obswVals(20);
-        % actuatorData.flags.flag_flight = logical(obswVals(21));
-        % actuatorData.flags.flag_ascent = logical(obswVals(22));
-        % actuatorData.flags.flag_burning = logical(obswVals(23));
-        % actuatorData.flags.flag_airbrakes = logical(obswVals(24));
-        % actuatorData.flags.flag_para1 = logical(obswVals(25));
-        actuatorData.flags.flag_para2 = logical(obswVals(26));
+    actuatorData.nas.n = obswVals(1);
+    actuatorData.nas.e = obswVals(2);
+    actuatorData.nas.d = obswVals(3);
+    actuatorData.nas.vn = obswVals(4);
+    actuatorData.nas.ve = obswVals(5);
+    actuatorData.nas.vd = obswVals(6);
+    actuatorData.nas.qx = obswVals(7);
+    actuatorData.nas.qy = obswVals(8);
+    actuatorData.nas.qz = obswVals(9);
+    actuatorData.nas.qw = obswVals(10);
+    actuatorData.nas.updating = obswVals(11);
+    actuatorData.actuators.parafoilLeftPercentage = obswVals(12);
+    actuatorData.actuators.parafoilRightPercentage = obswVals(13);
+    actuatorData.actuators.cutterState = obswVals(14);
+    actuatorData.wes.windX = obswVals(15);
+    actuatorData.wes.windY = obswVals(16);
+    actuatorData.guidance.psiRef = obswVals(17);
+    actuatorData.guidance.deltaA = obswVals(18);
+    actuatorData.guidance.currentTargetN = obswVals(19);
+    actuatorData.guidance.currentTargetE = obswVals(20);
+    % actuatorData.flags.flag_flight = logical(obswVals(21));
+    % actuatorData.flags.flag_ascent = logical(obswVals(22));
+    % actuatorData.flags.flag_burning = logical(obswVals(23));
+    % actuatorData.flags.flag_airbrakes = logical(obswVals(24));
+    % actuatorData.flags.flag_para1 = logical(obswVals(25));
+    actuatorData.flags.flag_para2 = logical(obswVals(26));
 
     % Receive data from serial comunication for motor
-        obswVals = serialbridge('Read','motor', 2);
-        
-        % actuatorData.actuators.mainValvePercentage = obswVals(1);
-        % actuatorData.actuators.ventingValvePercentage = obswVals(2);
+    obswVals = serialbridge('Read','motor', 2);
+    
+    actuatorData.actuators.mainValvePercentage = obswVals(1);
+    actuatorData.actuators.ventingValvePercentage = obswVals(2);
 
     
     % if the obsw sets flagFlight to true while the flag isLaunch is still
