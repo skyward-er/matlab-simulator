8fdb2b3d408173ced999c3142b65febf9257d9c8
Giuseppe Brentino <brentinogiuseppe@gmail.com>
2024-05-25 13:43:10 +0200

    [MEA] Rewritten MEA, still not working well for roccaraso

--- commonFunctions/kalman/run_MEA_LY.m
+++ commonFunctions/kalman/run_MEA_LY.m
@@ -1,113 +1,106 @@
-function [sensorDataMEA, sensorTotMEA] = run_MEA_LY(settingsMEA, sensorTotMEA, ...
-    sensorDataMEA, t_chambPress, t_nas, t_imu, MEAFrequency, NAS_states, S_ref, ...
-    combChambMeasures, imuAccelerometer, coeff_Cd,T1, CD_correction_shutDown, z0, N_prediction_threshold)
+function [sensorData,sensorTot] = run_MEA_LY(sensorData,sensorTot,settings,contSettings,u,T1)
 
-ign = 30;
+ign = 40;
 % mass estimation
-K_t = settingsMEA.K_t;
-V_e = settingsMEA.V_e;
-R_min = settingsMEA.Rs(1);
-R_max = settingsMEA.Rs(2);
+K_t = settings.mea.K_t;
+V_e = settings.mea.V_e+200;
+R_min = settings.mea.Rs(1);
+R_max = settings.mea.Rs(2);
 
 %define constants
 g = 9.81;
-
-dt = 1/MEAFrequency;
+dt = 0.02;
 alpha = (R_max - R_min)/(100^2-30^2);
 c = -alpha*30^2+R_min;
 
-% prediction
-
 % define time array for mea algorithm
-t_mea = sensorTotMEA.time(end):1/MEAFrequency:T1;
+t_mea = sensorTot.mea.time(end):1/settings.frequencies.MEAFrequency:T1;
+% define time array for sensors
+t_chambPress = sensorTot.comb_chamber.time;%(sensorTot.comb_chamber.time >= T1);
+t_nas = sensorTot.nas.time;%(sensorTot.nas.time>= T1); % we need also nas to estimate cd etc
+t_imu = sensorTot.imu.time;
 % initialise state update
-m = zeros(length(t_mea), 1);
-m(1) = sensorDataMEA.x(end);
-P(1) = sensorDataMEA.P(end);
-
+x(1,:) = sensorData.mea.estimated_mass(end);
+P(1) = sensorData.mea.P(end);
+predicted_apogee = 0;
 
 z_nas = zeros(length(t_mea), 1);
 vnorm_nas = zeros(length(t_mea), 1);
 vz_nas = zeros(length(t_mea), 1);
-z_nas(1) = NAS_states(end,3)  ;
-vnorm_nas(1) = norm(NAS_states(end,4:6));
-vz_nas(1) = NAS_states(end,6);
-
-index_chambPress = sum(t_mea(1) >= t_chambPress);
-index_NAS = sum(t_mea(1) >= t_nas);
+z_nas(1) = sensorData.nas.states(end,3);
+vnorm_nas(1) = norm(sensorData.nas.states(end,4:6));
+vz_nas(1) = sensorData.nas.states(end,6);
+index_chambPress = 0;
 
 for ii = 2:length(t_mea)
-    % retrieve NAS data
-    index_NAS = sum(t_mea(ii) >= t_nas);
-    z_nas(ii,1) = NAS_states(index_NAS,3);
-    vnorm_nas(ii,1) = norm(NAS_states(index_NAS,4:6));
-    vz_nas(ii,1) = NAS_states(index_NAS,6);
-
-    %propagation
     index_chambPress = sum(t_mea(ii) >= t_chambPress);
-    index_imu  = sum(t_mea(ii) >= t_imu);
-    if combChambMeasures(index_chambPress) > 1
-        m(ii) = m(ii-1) - combChambMeasures(index_chambPress)*K_t*dt/V_e;
+    index_imu = sum(t_mea(ii) >= t_imu);
+    index_nas = sum(t_mea(ii) >= t_nas);
 
+    z_nas(ii,1) = sensorTot.nas.states(index_nas,3);
+    vnorm_nas(ii,1) = norm(sensorTot.nas.states(index_nas,4:6));
+    vz_nas(ii,1) = sensorTot.nas.states(index_nas,6);
 
-        P(ii) = P(ii-1) + settingsMEA.Q ;
+    if sensorTot.comb_chamber.measures(index_chambPress)  > 1
+        % prediction
+        x(ii,:) = x(ii-1,:) - sensorTot.comb_chamber.measures(index_chambPress)*K_t*dt/V_e;
+        P(ii) = P(ii-1) + settings.mea.Q ;
+        if norm(sensorTot.imu.accelerometer_measures(index_imu, :)) > ign && vnorm_nas(ii) > 40
 
-        %correction
+            % model
+            cd = getDrag(vnorm_nas(ii), -z_nas(ii), 0, contSettings.coeff_Cd); %add correction shut_down??
+            [~,~,~, rho] = atmosisa(-z_nas(ii));
+            q = 0.5*rho*vnorm_nas(ii)^2; %dynamic pressure
+            F_a = q*settings.S*cd;        %aerodynamic force
 
-        cd = CD_correction_shutDown*getDrag(vnorm_nas(ii), -z_nas(ii), 0, coeff_Cd); %add correction shut_down??
-        [~,~,~, rho] = atmosisa(-z_nas(ii));
+            C2 = K_t * (sensorTot.comb_chamber.measures(index_chambPress)/x(ii)) - g - F_a/x(ii);
+            H = -K_t * sensorTot.comb_chamber.measures(index_chambPress)/x(ii)^2 + F_a/x(ii)^2;
 
+            %correction
+            R2 = (alpha*q + c);
+           
+            S = H*P(ii)*H' + R2;
+            if ~det(S)<1e-3
+                K = P(ii)*H' / S;
+                P(ii) = (1-K*H)*P(ii);
+                x(ii,:) = x(ii,:) + K.*(sensorTot.imu.accelerometer_measures(index_imu, 1) -g - C2);
+            end
 
-        q = 0.5*rho*vnorm_nas(ii)^2; %dynamic pressure
-        F_a = q*S_ref*cd;        %aerodynamic force
-
-        %linearized model
-        C2 = K_t .* (combChambMeasures(index_chambPress) ./ m(ii)) - g - F_a./m(ii);
-        H = -K_t .* combChambMeasures(index_chambPress)./m(ii).^2 + F_a ./ m(ii).^2;
-
-        R2 = (alpha*q + c);
-
-        S = H.*P(ii).*H + R2;
-
-        K = P(ii).*H ./ S;
-        P(ii) = (1-K*H)*P(ii);
-
-        if norm(imuAccelerometer(index_imu, :)) > ign && vnorm_nas(ii) > 40
-            m(ii) = m(ii) + K.*(imuAccelerometer(index_imu, 1) -g - C2);
         end
+
     else
-        m(ii) = m(ii);
+        x(ii,:) = x(ii-1,:);
     end
 
-end
+    % propagate apogee
+CD = settings.CD_correction_shutDown*getDrag(vnorm_nas(ii), -z_nas(ii), 0, contSettings.coeff_Cd); % coeffs potrebbe essere settings.coeffs
+[~,~,~,rho] = atmosisa(-z_nas(ii));
 
-
-% compute CD
-CD = CD_correction_shutDown*getDrag(vnorm_nas, -z_nas, 0, coeff_Cd);
-[~,~,~,rho] = atmosisa(-z_nas);
-
-propagation_steps = N_prediction_threshold - settingsMEA.counter_shutdown;
+propagation_steps = contSettings.N_prediction_threshold - settings.mea.counter_shutdown;
 if propagation_steps >=1
-    [z_pred, vz_pred] = PredictFuture(-z_nas,-vz_nas,  K_t .* combChambMeasures(index_chambPress), ...
-        S_ref, CD, rho, m, dt, propagation_steps);
+    [z_pred, vz_pred] = PredictFuture(-z_nas(ii),-vz_nas(ii), ...
+        K_t .* sensorTot.comb_chamber.measures(index_chambPress), ...
+        settings.S, CD, rho, x(ii,:), dt, propagation_steps);
 else
-    z_pred = -z_nas;
-    vz_pred = -vz_nas;
+    z_pred = -z_nas(ii);
+    vz_pred = -vz_nas(ii);
 end
 
-predicted_apogee = z_pred-z0 + 1./(2.*( 0.5.*rho .* CD * S_ref ./ m))...
-    .* log(1 + (vz_pred.^2 .* (0.5 .* rho .* CD .* S_ref) ./ m) ./ 9.81 );
+predicted_apogee(ii) = z_pred-settings.z0 + 1./(2.*( 0.5.*rho .* CD * settings.S ./ x(ii,:)))...
+    .* log(1 + (vz_pred.^2 .* (0.5 .* rho .* CD .* settings.S) ./ x(ii,:)) ./ 9.81 );
+
+end
+
+estimated_mass = x(:,1);
 
 % update local state
-sensorDataMEA.time = t_mea;
-sensorDataMEA.x = m;
-sensorDataMEA.P = P;
-sensorDataMEA.predicted_apogee = predicted_apogee;
+sensorData.mea.time = t_mea;
+sensorData.mea.P = P;
+sensorData.mea.predicted_apogee = predicted_apogee;
+sensorData.mea.estimated_mass = estimated_mass;
 
 % update total state
-sensorTotMEA.mass(sensorTotMEA.n_old:sensorTotMEA.n_old + length(sensorDataMEA.x)-2) = sensorDataMEA.x(2:end);
-sensorTotMEA.prediction(sensorTotMEA.n_old:sensorTotMEA.n_old + length(sensorDataMEA.x)-2) = sensorDataMEA.predicted_apogee(2:end);
-sensorTotMEA.time(sensorTotMEA.n_old:sensorTotMEA.n_old + length(sensorDataMEA.x)-2) = sensorDataMEA.time(2:end);
-sensorTotMEA.n_old = sensorTotMEA.n_old + size(m,1) - 1;
-
-end
+sensorTot.mea.mass(sensorTot.mea.n_old:sensorTot.mea.n_old + length(sensorData.mea.estimated_mass)-2) = sensorData.mea.estimated_mass(2:end);
+sensorTot.mea.prediction(sensorTot.mea.n_old:sensorTot.mea.n_old + length(sensorData.mea.estimated_mass)-2) = sensorData.mea.predicted_apogee(2:end);
+sensorTot.mea.time(sensorTot.mea.n_old:sensorTot.mea.n_old + length(sensorData.mea.estimated_mass)-2) = sensorData.mea.time(2:end);
+sensorTot.mea.n_old = sensorTot.mea.n_old + size(x,1) - 1;
