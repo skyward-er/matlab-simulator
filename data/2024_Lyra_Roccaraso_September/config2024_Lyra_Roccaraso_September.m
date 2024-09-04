3421962f58ab1cf9e07afcf5bb805e6f31a33a3c
Pier Francesco A. Bachini <pierfrancesco.bachini@skywarder.eu>
2024-09-04 23:41:11 +0200

    Update nas sigmas to better reflect swd implementation

--- data/2024_Lyra_Roccaraso_September/config2024_Lyra_Roccaraso_September.m
+++ data/2024_Lyra_Roccaraso_September/config2024_Lyra_Roccaraso_September.m
@@ -83,15 +83,15 @@ settings.servo.maxAngle = fzero(fun, deg2rad(50));
 settings.servo.maxAngle = fix(settings.servo.maxAngle*1e9)/1e9; % to avoid computational error propagation (truncates the angle to the ninth decimal)
 
 %% NAS TUNING PARAMETERS
-settings.nas.dt_k          =   0.02;                                    % [s]        nas time step
-settings.nas.sigma_baro    =  50;                                      % [Pa]   estimated barometer variance    
-settings.nas.sigma_mag     =   10;                                       % [mgauss^2] estimated magnetometer variance    
-settings.nas.sigma_GPS     =   diag([0.002 0.002 0.01/30 0.01/30]);               % [millideg^2 m^2/s^2]     estimated GPS variance. position from test, velocity from datasheet
-settings.nas.sigma_w       =   10;                                       % [rad^2/s^2]   estimated gyroscope variance;
-settings.nas.sigma_beta    =   1e-4;                                    % [rad/s^2]   estimated gyroscope bias variance;
+settings.nas.dt_k          =   0.02;                                        % [s]        nas time step
+settings.nas.sigma_baro    =   50;                                          % [Pa]   estimated barometer variance    
+settings.nas.sigma_mag     =   10;                                          % [mgauss] estimated magnetometer variance    
+settings.nas.sigma_GPS     =   sqrt(diag([0.002 0.002 0.01/30 0.01/30]));   % [millideg m/s]     estimated GPS variance. position from test, velocity from datasheet
+settings.nas.sigma_w       =   10;                                          % [rad/s]   estimated gyroscope variance;
+settings.nas.sigma_beta    =   1e-4;                                        % [rad/s]   estimated gyroscope bias variance;
 settings.nas.sigma_pitot   =   20^2;    
 settings.nas.sigma_pitot2  =   0.1; 
-settings.nas.sigma_acc     =   0.05;                                    % [m/s^2]
+settings.nas.sigma_acc     =   0.05;                                        % [m/s^2]
 settings.nas.P             = 0.01*eye(12);
 
 settings.nas.Mach_max = 0.4; % max mach number expected for the mission (for nas with pitot update purposes) - not currently used
