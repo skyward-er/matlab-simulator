44971bffa8a126f38a93e8210d5492ca4ba9264e
Nicolò Corbo <Nic.2002@yahoo.it>
2025-06-27 12:20:49 +0200

    [Pitot] Fixed Quaternions correction from sum to product correction, with addition of quatProd auxiliary function

--- commonFunctions/kalman/correctionPitot.m
+++ commonFunctions/kalman/correctionPitot.m
@@ -112,6 +112,8 @@ if cond(S) > threshold
     states_correction = (K*e)';                         %Kalman Correction Factor on MEKF States
 
     quat_correction = [0.5*states_correction(7:9), sqrt(1-0.25*states_correction(7:9)*states_correction(7:9)')]; % scalar last quaternions correction
+    quat_correction = quatProd(quat_correction', x_pred(7:10)')';
+
     correction = [states_correction(1:6), quat_correction, states_correction(10:12)]; % Correction vector for the states
 
     x    =   x_pred +correction;
@@ -123,4 +125,31 @@ end
 
 y_res =  e;                                             %Measurement residual vector
 
+end
+
+
+function quat = quatProd( quat1, quat2 )
+%	Calculates the Hemiltonian product between two quaternions
+%
+%   quat = quatProd( quat1, quat2 )
+%
+%   This function compute the hamiltonian product between two quaternions
+%   written as 4-by-1 vectors with the scalar component as the fourth
+%   element of the vector.
+%
+%   References:
+%	[1] Markley, F. Landis. "Attitude error representations for Kalman filtering." 
+%       Journal of guidance control and dynamics 26.2 (2003): 311-317.
+
+qv1 = quat1(1:3);
+qs1 = quat1(4);
+
+qv2 = quat2(1:3);
+qs2 = quat2(4); % scalar last in the hamiltonian product
+
+quat = [qs1 * qv2 + qs2 * qv1 - cross( qv1, qv2 ) ;
+              qs1 * qs2 - dot( qv1, qv2 )        ];
+
+quat = quat / norm(quat);
+          
 end
