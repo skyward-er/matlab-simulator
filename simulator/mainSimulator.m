0f52fdb717a490306efe95fce6abf0b655f9c3ba
Nicolò Corbo <Nic.2002@yahoo.it>
2025-07-03 11:13:42 +0200

    [Pitot] Reactivation of sensor_plots

--- simulator/mainSimulator.m
+++ simulator/mainSimulator.m
@@ -90,7 +90,7 @@ if ~exist("../commonFunctions/graphics/general-utilities/","dir")
     warning('To export file you need to download the repository, read the README file in the folder')
 end
  std_plots(simOutput,settings,contSettings,mission,environment)
-% sensor_plots(simOutput, environment, rocket, settings);
+ sensor_plots(simOutput, environment, rocket, settings);
 % report_plots(simOutput,settings,contSettings)
 
 %% state visualiser
